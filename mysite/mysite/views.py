08b3a5ff2a3fd56a2e9cf34c13703211385ab5a0
AKNursumar <a.k.nursumar@gmail.com>
2026-05-19 15:58:38 +0530

    Bootstrap used and textutils website is complete

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -21,43 +21,47 @@ def spaceremove(request):
 def Charcount(request):
     return HttpResponse("Char count")
 def analyze(request):
-    djtext = request.GET.get('text', 'default')
-    removepunc = request.GET.get('removepunc', 'off')
-    fullcaps = request.GET.get('fullcaps', 'off')
-    newlineremover = request.GET.get('newlineremover', 'off')
-    extraspaceremover = request.GET.get('extraspaceremover', 'off')
-    charcount = request.GET.get('charcount', 'off')
+    # Get the text
+    djtext = request.POST.get('text', 'default')
 
-    # print(djtext)
-    # print(removepunc)
-    analyzed = ""
-    punctuations = '''!"#$%&'()*+,-./:;<=>?@[]^_`{|}~'''
-    if removepunc == 'on':
+    removepunc = request.POST.get('removepunc', 'off')
+    fullcaps = request.POST.get('fullcaps', 'off')
+    newlineremover = request.POST.get('newlineremover', 'off')
+    extraspaceremover = request.POST.get('extraspaceremover', 'off')
+
+    if removepunc == "on":
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ""
         for char in djtext:
             if char not in punctuations:
-                analyzed += char
-        params = {'purpose':'remove punctuations','analyzed_text':analyzed}
-    elif fullcaps == 'on':
+                analyzed = analyzed + char
+        params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
+        djtext = analyzed
+        # return render(request, 'analyze.html', params)
+    if fullcaps == "on":
+        analyzed = ""
         for char in djtext:
-            analyzed += char.upper()
-        params = {'purpose': 'Change to Uppercase', 'analyzed_text': analyzed}
-    elif newlineremover == 'on':
+            analyzed = analyzed + char.upper()
+        params = {'purpose': 'Change To Uppercase', 'analyzed_text': analyzed}
+        djtext = analyzed
+        # return render(request, 'analyze.html', params)
+    if newlineremover == "on":
+        analyzed = ""
         for char in djtext:
-            if char != '/n':
-                analyzed += char
-        params = {'purpose': 'removed newline', 'analyzed_text': analyzed}
-    elif extraspaceremover == 'on':
-        for index,char in enumerate(djtext):
-            if not(djtext[index] == ' ' and djtext[index+1] == ' '):
-                analyzed += char
-        params = {'purpose': 'removed extra spaced', 'analyzed_text': analyzed}
-    elif charcount == 'on':
-        count = 0
-        for char in djtext:
-            count += 1
-        analyzed = str(count)
-        params = {'purpose': 'Count Characters', 'analyzed_text': analyzed}
-    else:
-        analyzed = djtext
-        params = {'purpose':'to analyze text','analyzed_text':analyzed}
-    return render(request,"analyze.html",params)
+            if char != "\n" and char !="\r":
+                analyzed = analyzed + char
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        # Analyze the text
+        djtext = analyzed
+        # return render(request, 'analyze.html', params)
+    if (extraspaceremover == "on"):
+        analyzed = ""
+        for index, char in enumerate(djtext):
+            if not (djtext[index] == " " and djtext[index + 1] == " "):
+                analyzed = analyzed + char
+        params = {'purpose': 'Change To Uppercase', 'analyzed_text': analyzed}
+        djtext = analyzed
+        # return render(request, 'analyze.html', params)
+    if(removepunc == "off" and fullcaps == "off" and extraspaceremover == "off" and newlineremover == "off"):
+        return HttpResponse(djtext + "</br>Nothing Changed")
+    return render(request, 'analyze.html', params)
